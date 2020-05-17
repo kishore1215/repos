9f2c50c29c9598970975af8b208bd208d64f0e53
kishore <kishoretrk@live.com>
2020-05-17 13:24:01 +0530

    .

--- hello_world/helloworld200517.py
+++ hello_world/helloworld200517.py
@@ -1,2 +1,12 @@
 print("Radha Soami")
 print('how much time i am going to take to learn all the things I want to learn')
+msg = 'radhasoami'
+print(msg.capitalize())
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,20,100)   # create list of evenly spaced numbers over space
+plt.plot(x, np.sin(x))      # plot sine of each x 
+plt.show()
+
